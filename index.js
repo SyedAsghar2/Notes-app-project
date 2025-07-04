5df58cbcc8b33caa41b9320c05b26519f2c9a93f
christopher altamirano <chrisalta94@gmail.com>
2025-07-03 21:37:40 -0400

    Added a delete and save function

--- index.js
+++ index.js
@@ -2,35 +2,72 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
-
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended:true}))
+const notesFile = __dirname + "/notes.json";
 
 
-//Static Files
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use('/css', express.static(__dirname + 'public/css'))
+app.use("/css", express.static(__dirname + "/public/css"));
 
 
-//Set Templating Engine
-app.set('views', './views')
-app.set('view engine', 'ejs')
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
 
-//Navigation
-app.get('/', (req, res) => {
-  res.render("index")
-})
+app.get("/", (req, res) => {
+  let notes = [];
+  try {
+    notes = JSON.parse(fs.readFileSync(notesFile));
+  } catch (err) {
+    console.error("Couldn't read notes.json:", err);
+  }
+  res.render("index", { notes });
+});
 
 
 app.post("/submit", (req, res) => {
-    console.log("Received data:", req.body);
-    res.send("Data received!")
-})
+  const { "note-title": title, "note-body": body } = req.body;
+
+  if (!title && !body) {
+    return res.status(400).send("Note is empty.");
+  }
+
+  let notes = [];
+  try {
+    notes = JSON.parse(fs.readFileSync(notesFile));
+  } catch (err) {
+    console.error("Couldn't read notes.json:", err);
+  }
+
+  notes.push({ id: Date.now(), title, body });
+
+  fs.writeFileSync(notesFile, JSON.stringify(notes, null, 2));
+  res.redirect("/");
+});
+
+
+app.post("/delete", (req, res) => {
+  const { noteId } = req.body;
+
+  let notes = [];
+  try {
+    notes = JSON.parse(fs.readFileSync(notesFile));
+  } catch (err) {
+    console.error("Couldn't read notes.json:", err);
+  }
+
+  const updatedNotes = notes.filter((n) => n.id !== parseInt(noteId));
+  fs.writeFileSync(notesFile, JSON.stringify(updatedNotes, null, 2));
+
+  res.redirect("/");
+});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
